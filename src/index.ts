53b1b0a3832d4a455db9ae0d51318b282cb757ec
ramonepico <ramonepico@gmail.com>
2023-10-16 13:23:50 -0300

    users e products

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-import { products, users } from "./database";
+import {
+  products,
+  users,
+  createUser,
+  createProduct,
+  getAllUsers,
+  getAllProducts,
+} from "./database";
+
+const newUser = {
+  id: "u003",
+  name: "Astrodev",
+  email: "astrodev@email.com",
+  password: "astrodev99",
+  createdAt: new Date().toISOString(),
+};
+
+const updatedUsers = createUser(newUser);
+
+const newProduct = {
+  id: "prod003",
+  name: "SSD gamer",
+  price: 349.99,
+  description:
+    "Acelere seu sistema com velocidades incríveis de leitura e gravação.",
+  imageUrl: "https://images.unsplash.com/photo",
+};
+
+const updatedProducts = createProduct(newProduct);
+
+//console.log(users);
+//console.log(products);
+//console.log(updatedUsers);
+//console.log(updatedProducts);
+console.log(getAllUsers());
+console.log(getAllProducts());
 
 
-console.log(users);
-console.log(products);
