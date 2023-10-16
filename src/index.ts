7e38e591429e18b99d6b472d0eee78c50b9ecfc0
ramonepico <ramonepico@gmail.com>
2023-10-16 15:54:25 -0300

    get e post users/products

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {
+/* import {
   products,
   users,
   createUser,
@@ -6,9 +6,9 @@ import {
   getAllUsers,
   getAllProducts,
   searchProductsByName
-} from "./database";
+} from "./database"; */
 
-const newUser = {
+/*const newUser = {
   id: "u003",
   name: "Astrodev",
   email: "astrodev@email.com",
@@ -39,5 +39,67 @@ const results = searchProductsByName("gamer");
 //console.log(updatedProducts);
 //console.log(getAllUsers());
 //console.log(getAllProducts());
-console.log(results);
+console.log(results); */
+
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { TProducts, TUsers} from './types'
+import { products, users } from './database'
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, () => {
+    console.log(`Servidor rodando na porta ${3003}`)
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('Pong!')
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  const result: TUsers[] = users;
+
+  res.status(200).send(result)
+})
+
+app.post("/users", (req: Request, res: Response) => {
+  const id = req.body.id as string; 
+  const name = req.body.name as string;
+  const email = req.body.email as string;
+  const password = req.body.password as string;
+  const createdAt = req.body.createdAt as string;
+
+  const newUser: TUsers = { id, name, email, password, createdAt };
+
+  users.push(newUser);
+
+  res.status(201).send("Cadastro realizado com sucesso");
+})
+
+app.get("/products", (req: Request, res: Response) => {
+  const result: TProducts[] = products;
+
+  res.status(200).send(result)
+})
+
+app.post("/products", (req: Request, res: Response) => {
+  const id = req.body.id as string;
+  const name = req.body.name as string; 
+  const price = req.body.price as number;
+  const description = req.body.description as string;
+  const imageUrl = req.body.imageUrl as string;
+
+  const newProduct: TProducts = { id, name, price, description, imageUrl };
+
+  products.push(newProduct);
+
+  res.status(201).send("Produto cadastrado com sucesso");
+})
+
+
+
 
